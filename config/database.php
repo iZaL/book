9b30408894f8f42f1ef753707889fb74146384b3
usama ahmed <uusa35@gmail.com>
2015-05-25 20:09:39 +0300

    first commit - elixir added with all bower componends

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
             'database'  => env('DB_DATABASE', 'homestead'),
-            'username'  => env('DB_USERNAME', 'root'),
+            'username'  => env('DB_USERNAME', 'homestead'),
             'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,35 @@
 var elixir = require('laravel-elixir');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Less
- | file for our application, as well as publishing vendor resources.
- |
- */
+elixir(function (mix)
+{
+ mix
+  // sass files in assets
+     .copy('public/bower_components/bootstrap-sass/assets', 'resources/assets/sass/bootstrap')
+  // css files in the public
+     .copy('public/bower_components/bootstrap-timepicker/css/bootstrap-timepicker.min.css','public/css/bootstrap-timepicker.min.css')
+  // js files
+     .copy('public/bower_components/jquery/dist/jquery.js', 'resources/assets/sass/bootstrap/javascripts/jquery.js')
+    // .copy('vendor/bower_components/bootstrap-sass/assets/javascripts', 'resources/assets/sass/vendor/bootstrap/javascripts')
+  //.copy('vendor/bower_components/bootstrap-timepicker/js/bootstrap-timepicker.min.js','resources/assets/sass/vendor/bootstrap/javascripts/bootstrap-timepicker.min.js')
+  // fonts
+     //.copy('vendor/bower_components/bootstrap-sass/assets/fonts/*', 'public/css/fonts')
+  // ... and so on
+ ;
 
-elixir(function(mix) {
-    mix.less('app.less');
+ mix.sass([
+  'app.scss' // this is file where I @import all vendor styles and my own styles
+ ]);
+
+ mix.scripts([
+  'sass/bootstrap/javascripts/jquery.js',
+  'sass/bootstrap/javascripts/bootstrap.js',
+  'sass/bootstrap/javascripts/bootstrap.min.js',
+  'sass/bootstrap/javascripts/bootstrap/',
+  //'resources/assets/sass/vendor/bootstrap/javascripts/bootstrap-timepicker.min.js',
+  //'app.js',
+ ], 'public/js/app.js','resources/assets/','./');
+ mix.version([
+  'public/css/app.css',
+  'public/js/app.js'
+ ]);
 });
